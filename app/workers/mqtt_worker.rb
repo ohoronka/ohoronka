a8b2f0c0952f845a19987846e8bd8796232af290
Bogdan Guban <biguban@gmail.com>
2017-06-20 17:32:20 +0300

    fix checking offline devices

--- app/workers/mqtt_worker.rb
+++ app/workers/mqtt_worker.rb
@@ -12,5 +12,6 @@ class MqttWorker
 
   def check_offline_devices(_params)
     Device.where(status: :online).where('pinged_at < ?', 30.seconds.ago).each &:offline!
+    MqttWorker.perform_in(30.seconds, :check_offline_devices, {}) unless Rails.env.test?
   end
 end
