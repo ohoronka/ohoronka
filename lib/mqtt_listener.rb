75e435ab210b20f778d799075560845386c4ba0d
Bogdan Guban <biguban@gmail.com>
2017-06-29 18:27:08 +0300

    added pid file for mqtt_listener

--- lib/mqtt_listener.rb
+++ lib/mqtt_listener.rb
@@ -1,7 +1,16 @@
 # TODO use configuration to pass server credentials
-MQTT::Client.connect('mqtt://test:test@localhost') do |c|
-  c.get('#') do |topic,message|
-    puts "#{topic}: #{message}"
-    MqttWorker.perform_async(:parse_message, topic: topic, message: message)
+begin
+  pid_file = File.expand_path('tmp/pids/mqtt_listener.pid', Rails.root)
+  File.open(pid_file, "w") do |f|
+    f.write Process.pid
   end
+
+  MQTT::Client.connect('mqtt://test:test@localhost') do |c|
+    c.get('#') do |topic,message|
+      puts "#{topic}: #{message}"
+      MqttWorker.perform_async(:parse_message, topic: topic, message: message)
+    end
+  end
+ensure
+  File.delete(pid_file) if File.exists?(pid_file)
 end
