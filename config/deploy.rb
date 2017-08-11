7ae3ddf18b91203b54aeef99cd1a75c422cc6db3
Bogdan Guban <biguban@gmail.com>
2017-08-11 07:31:23 +0300

    cable.yml was added to deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :bundle_flags, nil
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml", 'config/redis.yml', 'config/telegram.yml'
+append :linked_files, "config/database.yml", "config/secrets.yml", 'config/redis.yml', 'config/telegram.yml', 'config/cable.yml'
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
