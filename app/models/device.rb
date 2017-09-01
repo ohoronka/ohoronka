757e14e9bcf9d80e11887bdf78f61356943a7358
Bogdan Guban <biguban@gmail.com>
2017-09-01 12:12:22 +0300

    fix device password heneration.

--- app/models/device.rb
+++ app/models/device.rb
@@ -57,10 +57,19 @@ class Device < ApplicationRecord
     end
   end
 
+  # def password_hash(password, salt: nil, iterations: 901)
+  #   salt ||= SecureRandom.base64(12)
+  #   password_hash = Base64.encode64(PBKDF2.new(password: password, salt: salt, iterations: iterations).bin_string)[0..31]
+  #   "PBKDF2$sha256$#{iterations}$#{salt}$#{password_hash}"
+  # end
+
   def password_hash(password, salt: nil, iterations: 901)
     salt ||= SecureRandom.base64(12)
-    password_hash = Base64.encode64(PBKDF2.new(password: password, salt: salt, iterations: iterations).bin_string)[0..31]
-    "PBKDF2$sha256$#{iterations}$#{salt}$#{password_hash}"
+
+    hash = OpenSSL::PKCS5.pbkdf2_hmac(password, salt, iterations, 24, OpenSSL::Digest::SHA256.new)
+    encoded_hash = Base64.strict_encode64(hash)
+
+    "PBKDF2$sha256$#{iterations}$#{salt}$#{encoded_hash}"
   end
 
   private
