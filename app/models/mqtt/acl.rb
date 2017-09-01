ab79a56ac46e63bc39b9919443953238e5a65269
Bogdan Guban <biguban@gmail.com>
2017-09-01 17:28:29 +0300

    hot fix

--- app/models/mqtt/acl.rb
+++ app/models/mqtt/acl.rb
@@ -8,7 +8,7 @@ class Mqtt::Acl < ApplicationRecord
 
   belongs_to :mqtt_user, :class_name => 'Mqtt::User', inverse_of: :acls, foreign_key: :mqtt_user_id
 
-  before_save :set_user_name
+  before_validation :set_user_name
 
   validates :user_name, presence: true, uniqueness: {scope: :topic}
 
