b669eb4946239dd3c29cbe8c34a97d3da7eb09a3
Bogdan Guban <biguban@gmail.com>
2017-10-17 19:35:34 -0700

    service models were translated.

--- app/services/telegram_service.rb
+++ app/services/telegram_service.rb
@@ -21,7 +21,7 @@ class TelegramService
       when 'disable_alarm'
         channel = ::Channel::Telegram.find_by(identifier: message.from.id)
         channel.user.facilities.find(params[:facility_id]).disable_alarm
-        channel.notify('Alarm was disabled')
+        channel.notify(I18n.t('msg.alarm_was_disabled'))
       end
     when Telegram::Bot::Types::Message
       case message.text
@@ -31,10 +31,10 @@ class TelegramService
         channel = Channel::Telegram.find_by(auth_token: token)
         if channel
           channel.update(identifier: message.chat.id, activated: true)
-          channel.notify("Hey #{channel.user.decorate.full_name}! You have configured the Telegram Notification Channel")
+          channel.notify(I18n.t('channel.Telegram.was_configured'))
         else
           # TODO: notify dev
-          bot.api.send_message(chat_id: message.chat.id, text: "Hm... I can't connect you to any user account... Some thing went wrong...")
+          bot.api.send_message(chat_id: message.chat.id, text: t('msg.error'))
         end
       when '/start'
         # start without token
@@ -49,9 +49,9 @@ class TelegramService
       else
         channel = Channel::Telegram.find_by(identifier: message.chat.id)
         if channel
-          channel.notify("Hey #{channel.user.name}! I'm not so smart to answer your questions. So ask support guys on http://ohoronka.com/support")
+          channel.notify(I18n.t('channel.Telegram.not_so_smart', name: channel.user.decorate.full_name))
         else
-          bot.api.send_message(chat_id: message.chat.id, text: "Hey guy! I've no idea who you are. Please sign up to http://ohoronka.com")
+          bot.api.send_message(chat_id: message.chat.id, text: "Hey guy! I've no idea who you are. Please sign up to httpі://ohoronka.com")
         end
       end
     end
