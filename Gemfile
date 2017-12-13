395d6d5424c096aeb0cd9f50131022f96251d5e5
Bogdan Guban <biguban@gmail.com>
2017-12-14 00:30:16 +0200

    postgres was added.

--- Gemfile
+++ Gemfile
@@ -9,7 +9,11 @@ end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.0.rc2'
 # Use mysql as the database for Active Record
-gem 'mysql2', '>= 0.3.18', '< 0.5'
+# gem 'mysql2', '>= 0.3.18', '< 0.5'
+
+# Use postgresql as the database for Active Record
+gem 'pg', '~> 0.18'
+
 # Use Puma as the app server
 gem 'puma', '~> 3.7'
 # Use SCSS for stylesheets
--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,10 @@
 
 ActiveRecord::Schema.define(version: 20170927134637) do
 
-  create_table "channels", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "channels", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
@@ -23,61 +26,61 @@ ActiveRecord::Schema.define(version: 20170927134637) do
     t.index ["user_id"], name: "index_channels_on_user_id"
   end
 
-  create_table "devices", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "devices", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "pinged_at"
     t.bigint "facility_id"
     t.string "name"
-    t.integer "status", limit: 1, default: 6, null: false
+    t.integer "status", limit: 2, default: 6, null: false
     t.integer "gpio_listen", default: 0, null: false
     t.integer "gpio_pull", default: 0, null: false
     t.integer "gpio_ok", default: 0, null: false
     t.index ["facility_id"], name: "index_devices_on_facility_id"
   end
 
-  create_table "events", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "events", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "target_type"
     t.bigint "target_id"
     t.bigint "facility_id"
-    t.integer "target_status", limit: 1, default: 0, null: false
-    t.integer "facility_status", limit: 1, default: 0, null: false
+    t.integer "target_status", limit: 2, default: 0, null: false
+    t.integer "facility_status", limit: 2, default: 0, null: false
     t.index ["facility_id"], name: "index_events_on_facility_id"
     t.index ["target_type", "target_id"], name: "index_events_on_target_type_and_target_id"
   end
 
-  create_table "facilities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "facilities", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.integer "status", limit: 1, default: 1, null: false
+    t.integer "status", limit: 2, default: 1, null: false
   end
 
-  create_table "facility_shares", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "facility_shares", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.bigint "facility_id"
-    t.integer "role", limit: 1, default: 0
+    t.integer "role", limit: 2, default: 0
     t.index ["facility_id"], name: "index_facility_shares_on_facility_id"
     t.index ["user_id", "facility_id"], name: "index_facility_shares_on_user_id_and_facility_id", unique: true
     t.index ["user_id"], name: "index_facility_shares_on_user_id"
   end
 
-  create_table "friendships", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "friendships", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.bigint "friend_id"
-    t.integer "status", limit: 1, default: 0
+    t.integer "status", limit: 2, default: 0
     t.index ["friend_id"], name: "index_friendships_on_friend_id"
     t.index ["user_id", "friend_id"], name: "index_friendships_on_user_id_and_friend_id", unique: true
     t.index ["user_id"], name: "index_friendships_on_user_id"
   end
 
-  create_table "mqtt_acls", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "mqtt_acls", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "mqtt_user_id"
@@ -88,7 +91,7 @@ ActiveRecord::Schema.define(version: 20170927134637) do
     t.index ["user_name", "topic"], name: "index_mqtt_acls_on_user_name_and_topic", unique: true
   end
 
-  create_table "mqtt_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "mqtt_users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "device_id"
@@ -99,39 +102,39 @@ ActiveRecord::Schema.define(version: 20170927134637) do
     t.index ["user_name"], name: "index_mqtt_users_on_user_name", unique: true
   end
 
-  create_table "notifications", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "notifications", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.string "target_type"
     t.bigint "target_id"
-    t.integer "event", limit: 1, default: 0
+    t.integer "event", limit: 2, default: 0
     t.boolean "unread", default: true
     t.string "params"
     t.index ["target_type", "target_id"], name: "index_notifications_on_target_type_and_target_id"
     t.index ["user_id"], name: "index_notifications_on_user_id"
   end
 
-  create_table "sensors", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "sensors", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "device_id"
     t.string "name"
-    t.integer "status", limit: 1, default: 6, null: false
+    t.integer "status", limit: 2, default: 6, null: false
     t.integer "gpio_listen", default: 0, null: false
     t.integer "gpio_pull", default: 0, null: false
     t.integer "gpio_ok", default: 0, null: false
     t.index ["device_id"], name: "index_sensors_on_device_id"
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "first_name"
-    t.string "last_name"
     t.string "email"
     t.string "password_digest"
     t.boolean "admin", default: false
+    t.string "last_name"
     t.text "about"
     t.string "avatar"
     t.string "background"
