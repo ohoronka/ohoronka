1cea66e384c3bf82e694b3e0c790985c589da400
Bogdan Guban <biguban@gmail.com>
2018-01-14 12:16:40 +0200

    bugsnag js was added.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require bugsnag-js/dist/bugsnag.js
 //= require jquery3
 //= require popper
 //= require turbolinks
