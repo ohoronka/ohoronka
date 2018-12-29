2adc43cdf2c1e53de371e39ce6fa78be9f04e290
Bogdan Guban <biguban@gmail.com>
2018-12-29 21:39:08 +0200

    move sidekiq back

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,4 +1,4 @@
-require 'sidekiq/cli'
+# require 'sidekiq/cli'
 
 Sidekiq.configure_server do |config|
   config.redis = Ohoronka::Application::REDIS_CONFIG[:sidekiq]
@@ -8,15 +8,15 @@ Sidekiq.configure_client do |config|
   config.redis = Ohoronka::Application::REDIS_CONFIG[:sidekiq]
 end
 
-Thread.new do
-  begin
-    cli = Sidekiq::CLI.instance
-    cli.parse
-    cli.run
-  rescue => e
-    raise e if $DEBUG
-    STDERR.puts e.message
-    STDERR.puts e.backtrace.join("\n")
-    exit 1
-  end
-end
+# Thread.new do
+#   begin
+#     cli = Sidekiq::CLI.instance
+#     cli.parse
+#     cli.run
+#   rescue => e
+#     raise e if $DEBUG
+#     STDERR.puts e.message
+#     STDERR.puts e.backtrace.join("\n")
+#     exit 1
+#   end
+# end
