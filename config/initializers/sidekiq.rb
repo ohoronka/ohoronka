7a376418bea69e48fc11bb6a89ea30a86c1623d5
Bogdan Guban <biguban@gmail.com>
2018-12-29 21:02:38 +0200

    decrease memory usage

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,22 @@
+require 'sidekiq/cli'
+
 Sidekiq.configure_server do |config|
   config.redis = Ohoronka::Application::REDIS_CONFIG[:sidekiq]
 end
 
 Sidekiq.configure_client do |config|
   config.redis = Ohoronka::Application::REDIS_CONFIG[:sidekiq]
+end
+
+Thread.new do
+  begin
+    cli = Sidekiq::CLI.instance
+    cli.parse
+    cli.run
+  rescue => e
+    raise e if $DEBUG
+    STDERR.puts e.message
+    STDERR.puts e.backtrace.join("\n")
+    exit 1
+  end
 end
--- lib/mqtt_listener.rb
+++ lib/mqtt_listener.rb
@@ -1,10 +1,26 @@
+require 'sidekiq'
+require_relative 'app/workers/mqtt_worker'
+require 'erb'
+require 'yaml'
+require 'mqtt'
+
+env = ENV["RAILS_ENV"] || ENV["RACK_ENV"] || "development"
+
+redis_config = (YAML.load(ERB.new(File.read("config/redis.yml")).result) || {})[env] || {}
+mqtt_config = (YAML.load(ERB.new(File.read("config/mqtt.yml")).result) || {})[env] || {}
+
+Sidekiq.configure_client do |config|
+  config.redis = redis_config['sidekiq']
+end
+
+
 begin
-  pid_file = File.expand_path('tmp/pids/mqtt_listener.pid', Rails.root)
+  pid_file = File.expand_path('tmp/pids/mqtt_listener.pid', Dir.pwd)
   File.open(pid_file, "w") do |f|
     f.write Process.pid
   end
 
-  MQTT::Client.connect("mqtt://#{Mqtt.user_name}:#{Mqtt.password}@#{Mqtt.host}") do |c|
+  MQTT::Client.connect("mqtt://#{mqtt_config['user_name']}:#{mqtt_config['password']}@#{mqtt_config['host']}") do |c|
     c.get('#') do |topic,message|
       # puts "#{topic}: #{message}"
       MqttWorker.perform_async(:parse_message, topic: topic, message: message)
